ca2ce06b22a14dd1e87b9b25ac74864b9b8a3428
Jalal Uddin <jalal.exe@hotmail.com>
2015-01-18 15:53:07 +0600

    Added implementation for ShopOwnerRepository.cs

--- Code/ShopMaker/Membership/IUserAccount.cs
+++ Code/ShopMaker/Membership/IUserAccount.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using ShopMaker.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ShopMaker.Membership
 {
-	public interface IUserAccount 
+	public interface IUserAccount : IEntity
 	{
 		string EmailAddress { get;set; }
 
--- Code/ShopMaker/Membership/ShopOwnerRepository.cs
+++ Code/ShopMaker/Membership/ShopOwnerRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Common;
 
 namespace ShopMaker.Membership
 {
@@ -20,24 +21,57 @@ namespace ShopMaker.Membership
 
 		public virtual void Add(IUserAccount user)
 		{
-			throw new System.NotImplementedException();
+            DbCommand command = _dbCommandFactory.CreateCommand("ShopOwnerAccount_Add",
+                _dbCommandFactory.CreateParameter("AccountCreationDateTime", user.AccountCreationDateTime),
+                _dbCommandFactory.CreateParameter("AccountStatus", user.AccountStatus),
+                _dbCommandFactory.CreateParameter("EmailAddress", user.EmailAddress),
+                _dbCommandFactory.CreateParameter("FirstName", user.FirstName),
+                _dbCommandFactory.CreateParameter("IPAddress", user.IPAddress),
+                _dbCommandFactory.CreateParameter("LastLoginDateTime", user.LastLoginDateTime),
+                _dbCommandFactory.CreateParameter("LastName", user.LastName),
+                _dbCommandFactory.CreateParameter("LastWrongPasswordAttemptDateTime", user.LastWrongPasswordAttemptDateTime),
+                _dbCommandFactory.CreateParameter("MobileNumber", user.MobileNumber),
+                _dbCommandFactory.CreateParameter("Password", user.Password),
+                _dbCommandFactory.CreateParameter("WrongPasswordAttempt", user.WrongPasswordAttempt),
+                _dbCommandFactory.CreateParameter("ID", user.ID));
+
+            _dbCommandExecutionService.ExecuteCommand(command);
 		}
 
-		public virtual void Remove(string EmailAddress)
+		public virtual void Remove(string emailAddress)
 		{
-			throw new System.NotImplementedException();
+            DbCommand command = _dbCommandFactory.CreateCommand("ShopOwnerAccount_Remove",
+                _dbCommandFactory.CreateParameter("EmailAddress", emailAddress));
+
+            _dbCommandExecutionService.ExecuteCommand(command);
 		}
 
 		public virtual void Edit(IUserAccount user)
 		{
-			throw new System.NotImplementedException();
+            DbCommand command = _dbCommandFactory.CreateCommand("ShopOwnerAccount_Edit",
+                _dbCommandFactory.CreateParameter("AccountCreationDateTime", user.AccountCreationDateTime),
+                _dbCommandFactory.CreateParameter("AccountStatus", user.AccountStatus),
+                _dbCommandFactory.CreateParameter("EmailAddress", user.EmailAddress),
+                _dbCommandFactory.CreateParameter("FirstName", user.FirstName),
+                _dbCommandFactory.CreateParameter("IPAddress", user.IPAddress),
+                _dbCommandFactory.CreateParameter("LastLoginDateTime", user.LastLoginDateTime),
+                _dbCommandFactory.CreateParameter("LastName", user.LastName),
+                _dbCommandFactory.CreateParameter("LastWrongPasswordAttemptDateTime", user.LastWrongPasswordAttemptDateTime),
+                _dbCommandFactory.CreateParameter("MobileNumber", user.MobileNumber),
+                _dbCommandFactory.CreateParameter("Password", user.Password),
+                _dbCommandFactory.CreateParameter("WrongPasswordAttempt", user.WrongPasswordAttempt),
+                _dbCommandFactory.CreateParameter("ID", user.ID));
+
+            _dbCommandExecutionService.ExecuteCommand(command);
 		}
 
 		public virtual IUserAccount Get(string emailAddress)
 		{
-			throw new System.NotImplementedException();
-		}
+            DbCommand command = _dbCommandFactory.CreateCommand("ShopOwnerAccount_Get",
+                _dbCommandFactory.CreateParameter("EmailAddress", emailAddress));
 
+            return _dbCommandExecutionService.ExecuteQuery<ShopOwner>(command).FirstOrDefault<IUserAccount>();
+		}
 	}
 }
 
