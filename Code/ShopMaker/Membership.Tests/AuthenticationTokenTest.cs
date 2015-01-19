229735dca0ca89158505acbd349c2947b4265366
minhazAiub <minhazurrahman75@yahoo.com>
2015-01-20 00:46:41 +0600

    Not completed.Need a lot more practice to do it properly.

--- Code/ShopMaker/Membership.Tests/AuthenticationTokenTest.cs
+++ Code/ShopMaker/Membership.Tests/AuthenticationTokenTest.cs
@@ -26,6 +26,53 @@ namespace ShopMaker.Membership.Tests
 
             // act
             authenticationToken.EmailAddress = String.Empty;
+            Assert.AreEqual("email@email.com", authenticationToken.EmailAddress,"email is required");
+            
         }
+
+        public void EmailAddress_IsEmailAddressAlreadyUsed_ThrowsException()
+        {
+            //prepare
+            string usedEmail = "email@email.com";
+
+            var authenticationToken = _kernel.Get<IAuthenticationToken>();
+
+            // act
+            authenticationToken.EmailAddress=usedEmail;
+            Assert.AreEqual("email@email.com", authenticationToken.EmailAddress, "email is already used");
+        }
+
+        public void FirstName_EmptyFirstName_ThrowsException()
+        {
+            // prepare
+            var authenticationToken = _kernel.Get<IAuthenticationToken>();
+
+            // act
+            authenticationToken.FirstName = String.Empty;
+            Assert.AreEqual("fname", authenticationToken.FirstName, "First name is required");
+        }
+
+        public void LastName_EmptyLastName_ThrowsException()
+        {
+            // prepare
+            var authenticationToken = _kernel.Get<IAuthenticationToken>();
+
+            // act
+            authenticationToken.LastName = String.Empty;
+            Assert.AreEqual("Lname", authenticationToken.LastName, "Last name is required");
+           
+        }
+
+        //not completed
+        public void UserType_EmptyUserType_ThrowsException()
+        {
+            // prepare
+            var authenticationToken = _kernel.Get<IAuthenticationToken>();
+
+            // act
+            authenticationToken.UserType = UserTypeOptions.Admin;
+        }
+
+        
     }
 }
