fb3d1503efe37fcf270363f2d1e3bbba213e990a
mahbub-kuet <mrmanik.kuet@gmail.com>
2015-01-20 11:18:58 +0600

    Added some unit test.

--- Code/ShopMaker/Membership.Tests/ShopOwnerTest.cs
+++ Code/ShopMaker/Membership.Tests/ShopOwnerTest.cs
@@ -27,5 +27,87 @@ namespace ShopMaker.Membership.Tests
             // act
             shopOwnerAccount.MatchPassword(invalidPassword);
         }
+
+        public void MatchPassword_PlainPassword_ValidationResult()
+        {
+            // prepare
+            string PlainPassword = " ";
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+            bool IsValid = shopOwnerAccount.MatchPassword(PlainPassword);
+            Assert.Equals(true, IsValid);
+            // act
+            
+        }
+
+        public void ChangeMembershipPlan_NewPlan_CurrentPlanAsNewPlan()
+        {
+            // prepare
+
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+            
+
+            // act
+        }
+
+        public void EmailAddress_NullEmail_ThrowsException()
+        {
+            string invalidEmail = null;
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+            shopOwnerAccount.EmailAddress = invalidEmail;
+        }
+
+        public void EmailAddress_InvalidFormatEmail_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void EmailAddress_EmailAlreadyExist_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void Password_NullPassword_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void Password_TooShortPassword_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+        public void Password_TooLongPassword_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void FirstName_EmptyString_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void FirstName_TooLongName_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void LastName_EmptyString_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void LastName_TooLongName_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void MobileNumber_NullImput_ThrowsException()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
+
+        public void MobileNumber_InputNumber_ValidationResult()
+        {
+            IUserAccount shopOwnerAccount = _kernel.Get<IUserAccount>();
+        }
     }
 }
