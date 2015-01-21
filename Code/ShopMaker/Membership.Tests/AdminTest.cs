3d7c5b0549d413445e38f15198a2478549dd0059
anik <anik.islam022@gmail.com>
2015-01-21 22:51:55 +0600

    First test class for Admin Class

---
+++ Code/ShopMaker/Membership.Tests/AdminTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Proggasoft.Utility;
+using Moq;
+using Ninject;
+using Ninject.MockingKernel;
+using Ninject.MockingKernel.Moq;
+
+namespace ShopMaker.Membership.Tests
+{
+    [TestClass]
+    public class AdminTest : BaseTest
+    {
+        public AdminTest()
+        {
+            _kernel.Bind<IUserAccount>().To<Admin>();   
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MatchPassword_NullPassword_ThrowsException()
+        {
+            string emptyPassword = string.Empty;
+            IUserAccount adminAccount = _kernel.Get<IUserAccount>();
+
+            adminAccount.MatchPassword(emptyPassword);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EmailAddress_NullField_ThrowsException()
+        {
+            string emptyEmail = string.Empty;
+            IUserAccount adminAccount = _kernel.Get<IUserAccount>();
+
+            adminAccount.EmailAddress = emptyEmail;
+        }
+    }
+}
