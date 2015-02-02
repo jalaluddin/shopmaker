8e29bc5699134574e40adda8cbcc170dbd49e11a
Jalal Uddin <jalal.exe@hotmail.com>
2015-02-02 23:27:02 +0600

    added unit test for address

--- Code/ShopMaker/Membership.Tests/AddressTest.cs
+++ Code/ShopMaker/Membership.Tests/AddressTest.cs
@@ -176,168 +176,173 @@ namespace ShopMaker.Membership.Tests
             address.ID = invalidID;
         }
 
-
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Zipcode_getEmptyZipcode_ThrowsException()
+        public void Zipcode_GetZipcode_ReturnsNotNullZipcode()
         {
             // prepare
-            string invalidZipcode = string.Empty;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.Zipcode = invalidZipcode;
+            string zipcode = address.Zipcode;
+
+            // assert
+            Assert.IsNotNull(zipcode);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Zipcode_getNullZipcode_ThrowsException()
+        public void Zipcode_GetZipcode_ReturnsNonEmptyZipcode()
         {
             // prepare
-            string invalidZipcode = null;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.Zipcode = invalidZipcode;
+            string zipcode = address.Zipcode;
+
+            // assert
+            Assert.AreNotEqual(string.Empty, zipcode);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void StateOrPrvince_getNullStateOrPrvince_ThrowsException()
+        public void StateOrProvince_GetStateOrProvince_ReturnsNotNullStateOrProvince()
         {
             // prepare
-            string invalidStateOrPrvince = null;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.StateOrProvince = invalidStateOrPrvince;
-        }
+            string stateOrPrvince = address.StateOrProvince;
 
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void StateOrPrvince_getEmptyStateOrPrvince_ThrowsException()
-        {
-            // prepare
-            string invalidStateOrPrvince = string.Empty;
-            IAddress address = _kernel.Get<IAddress>();
-
-            // act
-            address.StateOrProvince = invalidStateOrPrvince;
-        }
-
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Country_getNullCountry_ThrowsException()
-        {
-            // prepare
-            string invalidCountry = null;
-            IAddress address = _kernel.Get<IAddress>();
-
-            // act
-            address.Country = invalidCountry;
-        }
-
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void Country_getEmptyCountry_ThrowsException()
-        {
-            // prepare
-            string invalidCountry = String.Empty;
-            IAddress address = _kernel.Get<IAddress>();
-
-            // act
-            address.Country = invalidCountry;
+            // assert
+            Assert.IsNotNull(stateOrPrvince);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void City_getEmptyCity_ThrowsException()
+        public void StateOrProvince_GetStateOrProvince_ReturnsNonEmptyStateOrProvince()
         {
             // prepare
-            string invalidCity = String.Empty;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.City = invalidCity;
-        }
+            string stateOrProvince = address.StateOrProvince;
 
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void City_getNullCity_ThrowsException()
-        {
-            // prepare
-            string invalidCity = null;
-            IAddress address = _kernel.Get<IAddress>();
-
-            // act
-            address.City = invalidCity;
+            // assert
+            Assert.AreNotEqual(string.Empty, stateOrProvince);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void AddressLine2_getNullAddressLine2_ThrowsException()
+        public void Country_GetCountry_ReturnsNotNullCountry()
         {
             // prepare
-            string invalidAddressLine2 = null;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.AddressLine2 = invalidAddressLine2;
+            string country = address.Country;
+
+            // assert
+            Assert.IsNotNull(country);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void AddressLine2_getEmptyAddressLine2_ThrowsException()
+        public void Country_GetCountry_ReturnsNonEmptyCountry()
         {
             // prepare
-            string invalidAddressLine2 = String.Empty;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.AddressLine2 = invalidAddressLine2;
+            string country = address.Country;
+
+            // assert
+            Assert.AreNotEqual(string.Empty, country);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void AddressLine1_getNullAddressLine1_ThrowsException()
+        public void City_GetCity_ReturnsNotNullCity()
         {
             // prepare
-            string invalidAddressLine1 = null;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.AddressLine1 = invalidAddressLine1;
+            string city = address.City;
+
+            // assert
+            Assert.IsNotNull(city);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void AddressLine1_getEmptyAddressLine1_ThrowsException()
+        public void City_GetCity_ReturnsNonEmptyCity()
         {
             // prepare
-            string invalidAddressLine1 = String.Empty;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.AddressLine1 = invalidAddressLine1;
-        }
+            string city = address.City;
 
+            // assert
+            Assert.AreNotEqual(string.Empty, city);
+        }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void ID_getEmptyID_ThrowsException()
+        public void AddressLine2_GetAddressLine2_ReturnsNotNullAddressLine2()
         {
             // prepare
-            Guid invalidID = Guid.Empty;
             IAddress address = _kernel.Get<IAddress>();
 
             // act
-            address.ID = invalidID;
+            string addressLine2 = address.AddressLine2;
+
+            // assert
+            Assert.IsNotNull(addressLine2);
         }
 
-        
+        [TestMethod]
+        public void AddressLine2_GetAddressLine2_ReturnsNonEmptyAddressLine2()
+        {
+            // prepare
+            IAddress address = _kernel.Get<IAddress>();
+
+            // act
+            string addressLine2 = address.AddressLine2;
+
+            // assert
+            Assert.AreNotEqual(string.Empty, addressLine2);
+        }
+
+        [TestMethod]
+        public void AddressLine1_GetAddressLine1_ReturnsNotNullAddressLine1()
+        {
+            // prepare
+            IAddress address = _kernel.Get<IAddress>();
+
+            // act
+            string addressLine1 = address.AddressLine1;
+
+            // assert
+            Assert.IsNotNull(addressLine1);
+        }
+
+        [TestMethod]
+        public void AddressLine1_GetAddressLine1_ReturnsNonEmptyAddressLine1()
+        {
+            // prepare
+            IAddress address = _kernel.Get<IAddress>();
+
+            // act
+            string addressLine1 = address.AddressLine1;
+
+            // assert
+            Assert.AreNotEqual(string.Empty, addressLine1);
+        }
+
+        [TestMethod]
+        public void ID_GetID_ReturnsNonEmptyID()
+        {
+            // prepare
+            IAddress address = _kernel.Get<IAddress>();
+
+            // act
+            Guid id = address.ID;
+
+            // assert
+            Assert.AreNotEqual(Guid.Empty, id);
+        }
     }
 }
